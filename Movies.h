d1d04d44b59670bbf6d9f663b3e1b654f75684ab
Jeffrey Teh <59184460+JTEH8@users.noreply.github.com>
2021-12-06 11:29:29 -0800

    Update Movies.h

--- Movies.h
+++ Movies.h
@@ -9,9 +9,9 @@ using namespace std;
 class Movies : public Media {
 public:
   int Duration = 0;
-  char* Director[100];
+  char Director[100];
   float Rating = 0;
-  Movies(char* newTitle, char* newDirector, int newYear, int newDuration, float newRating);
+  Movies(char newTitle, char newDirector, int newYear, int newDuration, float newRating);
   virtual void print();
 };
 
